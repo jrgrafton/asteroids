54de5d9e16da4ffb0966720025e8391a17ae0b63
jrgrafton <jrgrafton@gmail.com>
2014-03-15 12:50:31 +0000

    iPad tests

--- prototype/easelJS/js/init.js
+++ prototype/easelJS/js/init.js
@@ -195,7 +195,7 @@ var SpaceRocks = (function() {
 			// Create background
 			_this.background = new createjs.Shape();
 			alert(_this.width + " " + _this.height);
-			_this.background.graphics.beginFill("#00ff00").drawRect(0, 0, 1536, 1856);
+			_this.background.graphics.beginFill("#00ff00").drawRect(0, 0, _this.width, _this.height);
 			//_this.background.cache(-_this.width, -_this.height, (_this.width  * 2), (_this.height * 2));
 			_this.stage.addChild(_this.background);
 
