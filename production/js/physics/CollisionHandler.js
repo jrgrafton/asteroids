f0f6fe16c6cf2a472a7f2c38acf1d61dab771221
James Pollock <james@tictrac.com>
2014-07-16 10:18:55 +0100

    Remove nasty switch statements

--- production/js/physics/CollisionHandler.js
+++ production/js/physics/CollisionHandler.js
@@ -6,88 +6,73 @@ var CollisionHandler = (function() {
 		didCollide : function(e1, e2) {
 			//console.log("[collision] " + e1.className() + " --> " + e2.className());
 
-			switch(e1.className()) {
-				case "Asteroid":
-					switch(e2.className()) {
-						case "Missile":
-							// Explode missile
-							e2.explode();
-						break;
-						case "MissileExplosion":
-							// Explode asteroid
-							if(!e1.isDead()) {
-								e1.explode();
-								window.spaceRocks.addScore(10 * (e1.sizeIndex + 1), e2.x, e2.y);
-							}
-						break;
-						case "Player":
-							window.spaceRocks.addScore(10 * (e1.sizeIndex + 1), e2.x, e2.y);
-							e1.explode();
-							e2.explode();
-						break;
-						default:
-						break;
-					}
-					break;
-				break;
-				case "Player":
-					switch(e2.className()) {
-						case "Alien" :
-							// Explode player
-							e1.explode();
-						break;
-						case "Lazer" :
-							// Explode player
-							e1.explode();
-							e2.explode();
-						break;
-						default:
-						break;
-					}
-					break;
-				default:
-				break;
-				case "Alien" : {
-					switch(e2.className()) {
-						case "Missile" :
-							// Explode missile
-							e2.explode();
-						break;
-						case "MissileExplosion" :
-							// Explode alien
-							if(!e1.isDead()) {
-								e1.explode();
-								window.spaceRocks.addScore(120, e1.x, e1.y);
-							}
-						break;
-						case "Lazer" :
-							// Explode alien and lazer
-							e1.explode();
-							e2.explode();
-						break
-						default:
-						break;
-					}
-					break;
-				}
-				case "Lazer" : {
-					switch(e2.className()) {
-						case "Missile" :
-							// Explode lazer and missile
-							e1.explode();
-							e2.explode();
-						break;
-						case "MissileExplosion" :
-							// Explode alien
-							e1.explode();
-							e2.explode();
-						break;
-						default:
-						break;
-					}
-					break;
-				}
-			}
+            var collisionMap = {
+                "Asteroid": {
+                    "Missile": function (e1, e2) {
+                        // Explode missile
+                        e2.explode();
+                    },
+					"MissileExplosion": function (e1, e2) {
+                        // Explode asteroid
+                        if(!e1.isDead()) {
+                            e1.explode();
+                            window.spaceRocks.addScore(10 * (e1.sizeIndex + 1), e2.x, e2.y);
+                        }
+                    },
+					"Player": function (e1, e2) {
+                        window.spaceRocks.addScore(10 * (e1.sizeIndex + 1), e2.x, e2.y);
+                        e1.explode();
+                        e2.explode();
+                    }
+                },
+                "Player": {
+					"Alien": function (e1, e2) {
+                        // Explode player
+                        e1.explode();
+                    },
+                    "Lazer": function (e1, e2) {
+                        // Explode player
+                        e1.explode();
+                        e2.explode();
+                    }
+                },
+                "Alien": {
+                    "Missile": function (e1, e2) {
+                        // Explode missile
+                        e2.explode();
+                    },
+                    "MissileExplosion": function (e1, e2) {
+                        // Explode alien
+                        if(!e1.isDead()) {
+                            e1.explode();
+                            window.spaceRocks.addScore(120, e1.x, e1.y);
+                        }
+                    },
+					"Lazer": function (e1, e2) {
+                        // Explode alien and lazer
+                        e1.explode();
+                        e2.explode();
+                    }
+                },
+                "Lazer": {
+                    "Missile": function (e1, e2) {
+                        // Explode lazer and missile
+                        e1.explode();
+                        e2.explode();
+                    },
+                    "MissileExplosion": function (e1, e2) {
+                        // Explode alien
+                        e1.explode();
+                        e2.explode();
+                    }
+                }
+            };
+
+            if (e1.className() in collisionMap) {
+                if (e2.className() in collisionMap[e1.className()]) {
+                    collisionMap[e1.className()][e1.className()](e1, e2);
+                }
+            }
 		}
 	}
 
